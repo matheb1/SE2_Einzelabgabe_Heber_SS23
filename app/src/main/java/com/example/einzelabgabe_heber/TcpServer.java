1a7823fedc0a58be8ca6018838cea5fdce7cf361
matheb1 <101284481+matheb1@users.noreply.github.com>
2023-03-12 20:14:25 +0100

    added server, start

---
+++ app/src/main/java/com/example/einzelabgabe_heber/TcpServer.java
@@ -0,0 +1,7 @@
+package com.example.einzelabgabe_heber;
+
+public class TcpServer {
+    public static final int SERVER_PORT		= 53212;
+    public static final String SERVER_STRING = "se2-isys.aau.at";
+
+}
