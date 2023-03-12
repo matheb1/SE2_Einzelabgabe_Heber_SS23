1c2e975f6e54448a82031da06ed9999cd9ec1cff
matheb1 <101284481+matheb1@users.noreply.github.com>
2023-03-12 20:07:11 +0100

    Added the needed code for getting the input on java file

--- app/src/main/java/com/example/einzelabgabe_heber/MainActivity.java
+++ app/src/main/java/com/example/einzelabgabe_heber/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.einzelabgabe_heber;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String input = editText.getText().toString();
+            }
+        });
+
     }
 }
