c398ff1297f4cb1dbcf888c1e29a9ba4837eb3be
matheb1 <101284481+matheb1@users.noreply.github.com>
2023-03-12 20:28:52 +0100

    added tcp client

---
+++ app/src/main/java/com/example/einzelabgabe_heber/TcpClient.java
@@ -0,0 +1,43 @@
+package com.example.einzelabgabe_heber;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class TcpClient {
+    public static final int SERVER_PORT		= 53212;
+    public static final String SERVER_STRING = "se2-isys.aau.at";
+
+    private String doInBackground(String... strings){
+
+        String message = strings[0];
+        String response = null;
+
+        try {
+            Socket socket = new Socket(SERVER_STRING, SERVER_PORT);
+            OutputStream os = socket.getOutputStream();
+            InputStream is = socket.getInputStream();
+
+            byte[] messageBytes = message.getBytes();
+            os.write(messageBytes, 0, messageBytes.length);
+            os.flush();
+
+            byte[] buffer = new byte[1024];
+            int bytesRead = is.read(buffer);
+            response = new String(buffer, 0, bytesRead);
+
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return response;
+
+    }
+
+
+
+
+}
--- app/src/main/java/com/example/einzelabgabe_heber/TcpServer.java
+++
@@ -1,7 +0,0 @@
-package com.example.einzelabgabe_heber;
-
-public class TcpServer {
-    public static final int SERVER_PORT		= 53212;
-    public static final String SERVER_STRING = "se2-isys.aau.at";
-
-}
